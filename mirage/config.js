4d67b51e92169a25026def1373a5c5f37e0c23b3
Antonio <antonioalvino95@gmail.com>
2019-05-10 11:23:48 +0200

    Setting Mirage for faking values and paths config

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,12 @@
 export default function() {
 
-  // These comments are here to help you get started. Feel free to delete them.
+  this.urlPrefix = 'http://localhost:8080';
+  this.namespace = '/api';    
 
-  /*
-    Config (with defaults).
+    this.get('/adverts');
+    this.post('/adverts');
+    this.get('/adverts/:id');
+    this.patch('/adverts/:id');
+    this.del('/adverts/:id');
 
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.4.x/shorthands/
-  */
 }
--- mirage/factories/advert.js
+++ mirage/factories/advert.js
@@ -1,4 +1,22 @@
-import { Factory } from 'ember-cli-mirage';
+import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
+        title() {
+          return faker.commerce.product();
+        },
+        fuel() {
+          return faker.commerce.color();
+        },
+        price() {
+          return faker.random.number();
+        },
+        isNew() {
+          return faker.random.boolean();
+        },
+        mileage() {
+          return faker.random.number();
+        },
+        firstRegistration() {
+            return faker.date.past(1);
+          }
 });
