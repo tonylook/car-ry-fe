fbb2e34bc98ef8afd7546ee0ba8da2ba53098d21
Antonio <antonioalvino95@gmail.com>
2019-05-10 10:53:34 +0200

    Model definition, and mirage model generation

---
+++ app/models/advert.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    //id: DS.attr('number'),
+    title: DS.attr('string'),
+    fuel: DS.attr('string'),
+    price: DS.attr('number'),
+    isNew: DS.attr('boolean'),
+    mileage: DS.attr('number'),
+    firstRegistration: DS.attr('date')
+});
